642f61b2a48ce86d333e869fada2825e814262b8
yangyonghui <yangyonghui@secoo.com>
2017-08-11 11:25:57 +0800

    Initial Commit

---
+++ ShareDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareDemo
+//
+//  Created by secoo_yonghui on 2017/8/11.
+//  Copyright © 2017年 secoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareDemo
+//
+//  Created by secoo_yonghui on 2017/8/11.
+//  Copyright © 2017年 secoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

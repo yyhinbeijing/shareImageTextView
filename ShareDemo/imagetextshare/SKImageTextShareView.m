8de8fe560be9ec7bc9aeaca25d9377b1a157f451
yangyonghui <yangyonghui@secoo.com>
2017-08-14 10:09:29 +0800

    优化demo展示效果更佳

--- ShareDemo/imagetextshare/SKImageTextShareView.m
+++ ShareDemo/imagetextshare/SKImageTextShareView.m
@@ -9,6 +9,7 @@
 #import "UIButton+Tool.h"
 #import "UIView+Title.h"
 #import "UIView+LZFrame.h"
+#import "UIImageView+WebCache.h"
 
 
 @implementation SKImageTextShareView
@@ -19,6 +20,58 @@
     if (self) {
         float height = SCREENHEIGHT - 60;
         CrazyBasisView *view = [[CrazyBasisView alloc]initWithFrame:CGRectMake(0, SCREENHEIGHT, SCREENWIDTH, height)];
+        
+        
+        CGFloat backViewRightPadding = 40;
+        CGFloat backViewViewLeftPadding = 40;
+        CGFloat backViewViewTopPadding = 40;
+        CGFloat backViewWidth = SCREENWIDTH - backViewRightPadding - backViewViewLeftPadding;
+        CGFloat backViewHeight = backViewWidth * 1.5;
+        UIImageView *backView = [[UIImageView alloc] initWithFrame:CGRectMake(backViewViewLeftPadding, backViewViewTopPadding, backViewWidth, backViewHeight)];
+//        [backView sd_setImageWithURL:nil placeholderImage:[UIImage imageNamed:@"a"]];
+        [view addSubview:backView];
+        backView.layer.masksToBounds = YES;
+        backView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        backView.layer.borderWidth = 2;
+        
+        CGFloat goodImageViewRightPadding = 0;
+        CGFloat goodImageViewLeftPadding = 0;
+        CGFloat goodImageViewTopPadding = 0;
+        CGFloat goodImageViewWidth = backViewWidth - goodImageViewRightPadding - goodImageViewLeftPadding;
+        CGFloat goodImageViewHeight = goodImageViewWidth;
+        UIImageView *goodImageView = [[UIImageView alloc] initWithFrame:CGRectMake(goodImageViewLeftPadding, goodImageViewTopPadding, goodImageViewWidth, goodImageViewHeight)];
+//        [ sd_setImageWithURL:nil placeholderImage:];
+        goodImageView.image = [UIImage imageNamed:@"a2x@"];
+        [backView addSubview:goodImageView];
+        
+        
+        CGFloat qrCodeViewLeftPadding = 0;
+        CGFloat qrCodeViewTopPadding = 0;
+        CGFloat qrCodeViewwidth = 20;
+        CGFloat qrCodeViewHeight = 20;
+        UIImageView *qrCodeView = [[UIImageView alloc] initWithFrame:CGRectMake(qrCodeViewLeftPadding, goodImageView.lz_bottom + qrCodeViewTopPadding, qrCodeViewwidth, qrCodeViewHeight)];
+//        [qrCodeView sd_setImageWithURL:nil placeholderImage:];
+        qrCodeView.image = [UIImage imageNamed:@"uuuuuuuu@"];
+        [backView addSubview:goodImageView];
+        
+        CGFloat brandAndInfoLeftPadding = 20;
+        CGFloat brandAndInfoTopPadding = 23;
+        CGFloat brandAndInfoWidth = 100;
+        CGFloat brandAndInfoHeight = 23;
+        UILabel *brandAndInfo = [[UILabel alloc] initWithFrame:CGRectMake(qrCodeView.lz_x + brandAndInfoLeftPadding, goodImageView.lz_bottom + brandAndInfoTopPadding, brandAndInfoWidth, brandAndInfoHeight)];
+        brandAndInfo.text = @"苹果/的点点滴滴多多多多多多多多";
+        [backView addSubview:brandAndInfo];
+        
+        
+        CGFloat priceLabelLeftPadding = 20;
+        CGFloat priceLabelTopPadding = 23;
+        CGFloat priceLabelWidth = 100;
+        CGFloat priceLabelHeight = 23;
+        UILabel *priceLabel = [[UILabel alloc] initWithFrame:CGRectMake(qrCodeView.lz_x + priceLabelLeftPadding, brandAndInfo.lz_bottom + priceLabelTopPadding, priceLabelWidth, priceLabelHeight)];
+        priceLabel.text = @"100000";
+        [backView addSubview:priceLabel];
+        
+        
         view.tag = 1;
         view.backgroundColor = [UIColor whiteColor];
         float interval = 35;
